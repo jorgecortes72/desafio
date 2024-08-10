7db956f028bf048c0a83b7ffc19770633252c6c4
jorgecortes72 <jorge.cortes.arica@gmail.com>
2024-08-09 23:02:11 -0400

    Encriptador e Index modificados: ahora encripta y desencrpta

--- encriptador/encriptador.js
+++ encriptador/encriptador.js
@@ -15,6 +15,9 @@ La letra "u" es convertida para "ufat"
 // let llaveEncriptacion=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
 console.log(mensajeIngresado);
 
+
+// BLOQUE ENCRIPTAR 
+
 function btnEncriptar(mensajeIngresado){
     // const mensajeIngresado= document.querySelector(".campo-mensaje-ingresado");
     //console.log('holis')
@@ -36,22 +39,71 @@ function encriptar(mensajeIngresado){
 
     let llaveEncriptacion=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
 
-    // console.table(llaveEncriptacion);
+    console.table(llaveEncriptacion);
 
-    for (j=1; j <=mensajeIngresado.length;j++){
+    // for (j=1; j <=mensajeIngresado.length;j++){
 
-        for (i=0; i <= llaveEncriptacion.length; i++){
+        for (i=0; i < llaveEncriptacion.length; i++){
 
             console.log(mensajeIngresado[[i][0]]);
-            console.log(mensajeIngresado[[j][0]]);
+            // console.log(mensajeIngresado[[j][0]]);
+            console.log(llaveEncriptacion[i][0]);
 
-            if(mensajeIngresado.includes(llaveEncriptacion[[i][0]])){
+            if(mensajeIngresado.includes(llaveEncriptacion[i][0])){
 
-                mensajeIngresado= mensajeIngresado.replaceAll((llaveEncriptacion[[i][0]]), (llaveEncriptacion[[i][1]]));
-                console.log(mensajeIngresado[[i][1]]);
+                console.log(i);
+                mensajeIngresado= mensajeIngresado.replaceAll(llaveEncriptacion[i][0], llaveEncriptacion[i][1]);
+                console.log(mensajeIngresado[i][1]);
             }
         }
-    }
+    // }
+    // console.log(mensajeIngresado);
+    return mensajeIngresado;
+
+}
+
+// BLOQUE DESENCRIPTAR 
+
+
+function btnDesencriptar(mensajeIngresado){
+    // const mensajeIngresado= document.querySelector(".campo-mensaje-ingresado");
+    //console.log('holis')
+    //console.log(mensajeIngresado.value);
+
+    const textoEncriptado=desencriptar(mensajeIngresado.value);
+    mensajeSalida.value= textoEncriptado;
+    mensajeSalida.style.backgroundImage="none";
+    
+    console.log(mensajeSalida);
+}
+
+
+
+function desencriptar(mensajeIngresado){
+
+    mensajeIngresado = mensajeIngresado.toLowerCase();
+    //console.log(mensajeIngresado);
+
+    let llaveEncriptacion=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    console.table(llaveEncriptacion);
+
+    // for (j=1; j <=mensajeIngresado.length;j++){
+
+        for (i=0; i < llaveEncriptacion.length; i++){
+
+            console.log(mensajeIngresado[[i][0]]);
+            // console.log(mensajeIngresado[[j][0]]);
+            console.log(llaveEncriptacion[i][0]);
+
+            if(mensajeIngresado.includes(llaveEncriptacion[i][1])){
+
+                console.log(i);
+                mensajeIngresado= mensajeIngresado.replaceAll(llaveEncriptacion[i][1], llaveEncriptacion[i][0]);
+                console.log(mensajeIngresado[i][1]);
+            }
+        }
+    // }
     // console.log(mensajeIngresado);
     return mensajeIngresado;
 
